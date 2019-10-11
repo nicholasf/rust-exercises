0336c3844d289d67b7a0fc918986b32e7c93f906
Nicholas Faiz <nicholas.faiz@gmail.com>
2019-10-11 11:05:03 +1100

    Small steps

--- file-parsing/src/main.rs
+++ file-parsing/src/main.rs
@@ -1,38 +1,50 @@
 use std::env;
 
 use std::io;
-use std::io::prelude::*;
 use std::io::BufReader;
+
 use std::fs::File;
+use std::fs::OpenOptions;
+
+use std::path::PathBuf;
 
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() <= 2 {
-        println!("\n\n Please supply a search term!");
+    println!("args len {}", args.len());
+
+    if args.len() < 2 {
+        println!("\n\n Please supply a file to open!");
         process::exit(0);
     }
+
+    let filePath: &str = args[1].trim();
+    let path: PathBuf = PathBuf::from(filePath.to_string());
     
-    let term: &str = args[1].trim();
-    parse(&*term);
+
+    let file = match open_file(path) {
+        Ok(file) => file,
+        Err(e) => panic!("This file is problematic. {}", e),    
+    };
+
 }
 
-fn parse(term: &str) -> io::Result<()> {
-    let cwd = env::current_dir()?;
-    let ulysses = cwd.join("data/ulysses.txt");
-    let f = File::open(ulysses)?;
-    let reader = BufReader::new(f);
-
-    for line in reader.lines() {
-        let l = line?;
-
-        if l.contains(term) {
-            println!("{}", l);
-        }
-    }
-
-    Ok(())
+fn open_file(path: PathBuf) -> io::Result<File> {
+    return OpenOptions::new().read(true).open(path);
 }
+
+// fn parse(term: &str) -> io::Result<()> {
+
+//     for line in reader.lines() {
+//         let l = line?;
+
+//         if l.contains(term) {
+//             println!("{}", l);
+//         }
+//     }
+
+//     Ok(())
+// }
 
